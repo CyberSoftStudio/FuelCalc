0a133c6d3dad8e6f67791a7fc2a54e9adafe3378
EmilProkopov <emmanuilprokopov@gmail.com>
2018-08-07 18:00:46 +0300

    Animation for buttons

--- app/src/main/java/com/example/user2/fuelcalc/ui/SettingsActivity.java
+++ app/src/main/java/com/example/user2/fuelcalc/ui/SettingsActivity.java
@@ -4,27 +4,17 @@ import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.content.SharedPreferences;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.RotateAnimation;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
 import com.example.user2.fuelcalc.R;
 import com.example.user2.fuelcalc.fuels.FuelType;
@@ -40,6 +30,10 @@ public class SettingsActivity extends AppCompatActivity
 
 
     private static final String LOGTAG = "SettingsActivity";
+    private int oldTextColorMode;
+    private int oldBackgroundColorMode;
+    private int textColorMode;
+    private int backgroundColorMode;
 
     private FuelModel fuelModel;
     Switch switcher;
@@ -49,6 +43,7 @@ public class SettingsActivity extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
@@ -58,7 +53,6 @@ public class SettingsActivity extends AppCompatActivity
         switcher = findViewById(R.id.switcher);
         switcher.setChecked(sharedPreferences.getBoolean("night_mode", false));
         switcher.setOnCheckedChangeListener(this);
-
 
         arrowBackAlphaAnimator = ValueAnimator.ofFloat(0f, 1f);
         arrowBackAlphaAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
@@ -71,7 +65,6 @@ public class SettingsActivity extends AppCompatActivity
         arrowBackAlphaAnimator.setDuration(250);
         arrowBackAlphaAnimator.setRepeatMode(ValueAnimator.REVERSE);
         arrowBackAlphaAnimator.setRepeatCount(0);
-
 
         changeTheme(sharedPreferences.getBoolean("night_mode", false));
 
@@ -93,15 +86,25 @@ public class SettingsActivity extends AppCompatActivity
         colorAnim.start();
     }
 
+    private void animateClick(View aimView) {
+
+        ValueAnimator colorAnim = ObjectAnimator.ofInt(aimView,
+                "backgroundColor", getResources().getColor(R.color.EarlGray),
+                getResources().getColor(R.color.invisible));
+        colorAnim.setDuration(150);
+        colorAnim.setEvaluator(new ArgbEvaluator());
+        colorAnim.setRepeatCount(0);
+        colorAnim.setRepeatMode(ValueAnimator.REVERSE);
+        colorAnim.start();
+    }
 
     private void showSnackbar(View background, String message) {
+
         Snackbar snackbar = Snackbar
                 .make(background, message, Snackbar.LENGTH_SHORT);
-
         int snackbarTextId = android.support.design.R.id.snackbar_text;
         TextView tv = snackbar.getView().findViewById(snackbarTextId);
         tv.setTextColor(getResources().getColor(R.color.White));
-
         snackbar.show();
     }
 
@@ -112,13 +115,14 @@ public class SettingsActivity extends AppCompatActivity
 
 
     public void onClickAddBtn(View v) {
+        animateClick(v);
         DialogFragment dialog = new AddFuelDialogFragment();
         dialog.show(getSupportFragmentManager(), "AddFuelDialogFragment");
     }
 
 
     public void onClickResetDefaultBtn(View v) {
-
+        animateClick(v);
         DialogFragment dialog = new ConfirmResetDialogFragment();
         dialog.show(getSupportFragmentManager(), "ConfirmResetDialogFragment");
     }
@@ -128,17 +132,13 @@ public class SettingsActivity extends AppCompatActivity
     public void onCheckedChanged(CompoundButton compoundButton, boolean nightMode) {
 
         editor = sharedPreferences.edit();
-
         if (nightMode) {
             editor.putBoolean("night_mode", true);
         } else {
             editor.putBoolean("night_mode", false);
         }
-
         changeTheme(nightMode);
-
         editor.apply();
-
     }
 
 
@@ -146,12 +146,10 @@ public class SettingsActivity extends AppCompatActivity
     public void onAddFuelDialogPositiveClick(DialogFragment dialog, String fuelName,
                                              String unitName, String caloricity, String price) {
 
-
         if (fuelName.equals("") || unitName.equals("") || caloricity.equals("") || price.equals("")) {
             showToast("All input fields must not be empty");
             return;
         }
-
 
         List<FuelType> fuelTypes = fuelModel.getFuelTypes();
         for (FuelType curFuelType : fuelTypes) {
@@ -161,26 +159,20 @@ public class SettingsActivity extends AppCompatActivity
             }
         }
 
-
         double priceDouble, caloricityDouble;
-
         try {
             priceDouble = Double.parseDouble(price);
             caloricityDouble = Double.parseDouble(caloricity);
-
         } catch (NumberFormatException e) {
             showToast("\"Caloricity\" and \"price\" fields must contain only numbers");
             return;
         }
 
-
         fuelModel.addFuel(new FuelType(fuelName, unitName, -1.0, priceDouble, -1.0,
                 caloricityDouble));
-
         fuelModel.saveData();
 
         dialog.getDialog().cancel();
-
         showSnackbar(findViewById(R.id.settings_layout), "New fuel added");
     }
 
@@ -208,76 +200,46 @@ public class SettingsActivity extends AppCompatActivity
     private void changeTheme(boolean nightMode) {
 
         if (nightMode) {
-            animateBase(findViewById(R.id.settings_layout), "backgroundColor",
-                    getResources().getColor(R.color.White),
-                    getResources().getColor(R.color.Coal));
+            oldTextColorMode = getResources().getColor(R.color.Black);
+            oldBackgroundColorMode = getResources().getColor(R.color.White);
+            textColorMode = getResources().getColor(R.color.White);
+            backgroundColorMode = getResources().getColor(R.color.Coal);
 
-            animateBase(findViewById(R.id.nmText), "textColor",
-                    getResources().getColor(R.color.Black),
-                    getResources().getColor(R.color.White));
-
-            animateBase(findViewById(R.id.actonbar_title), "textColor",
-                    getResources().getColor(R.color.Black),
-                    getResources().getColor(R.color.White));
-
-            animateBase(findViewById(R.id.reset_fuel_btn), "textColor",
-                    getResources().getColor(R.color.Black),
-                    getResources().getColor(R.color.White));
-
-            animateBase(findViewById(R.id.add_fuel_btn), "textColor",
-                    getResources().getColor(R.color.Black),
-                    getResources().getColor(R.color.White));
-
-            animateBase(findViewById(R.id.help_button), "textColor",
-                    getResources().getColor(R.color.Black),
-                    getResources().getColor(R.color.White));
-
-            animateBase(findViewById(R.id.about_button), "textColor",
-                    getResources().getColor(R.color.Black),
-                    getResources().getColor(R.color.White));
             ((ImageView) findViewById(R.id.actonbar_icon))
                     .setImageResource(R.drawable.ic_arrow_back_white_24dp);
-
-            arrowBackAlphaAnimator.start();
-
         } else {
-            animateBase(findViewById(R.id.settings_layout), "backgroundColor",
-                    getResources().getColor(R.color.Coal),
-                    getResources().getColor(R.color.White));
-
-            animateBase(findViewById(R.id.actonbar_title), "textColor",
-                    getResources().getColor(R.color.White),
-                    getResources().getColor(R.color.Black));
-
-            animateBase(findViewById(R.id.reset_fuel_btn), "textColor",
-                    getResources().getColor(R.color.White),
-                    getResources().getColor(R.color.Black));
-
-            animateBase(findViewById(R.id.add_fuel_btn), "textColor",
-                    getResources().getColor(R.color.White),
-                    getResources().getColor(R.color.Black));
-
-            animateBase(findViewById(R.id.help_button), "textColor",
-                    getResources().getColor(R.color.White),
-                    getResources().getColor(R.color.Black));
-
-            animateBase(findViewById(R.id.about_button), "textColor",
-                    getResources().getColor(R.color.White),
-                    getResources().getColor(R.color.Black));
-
-            animateBase(findViewById(R.id.nmText), "textColor",
-                    getResources().getColor(R.color.White),
-                    getResources().getColor(R.color.Black));
+            oldTextColorMode = getResources().getColor(R.color.White);
+            oldBackgroundColorMode = getResources().getColor(R.color.Coal);
+            textColorMode = getResources().getColor(R.color.Black);
+            backgroundColorMode = getResources().getColor(R.color.White);
 
             ((ImageView) findViewById(R.id.actonbar_icon))
                     .setImageResource(R.drawable.ic_arrow_back_black_24dp);
-
-
-            ((ImageView) findViewById(R.id.actonbar_icon))
-                    .setImageResource(R.drawable.ic_arrow_back_black_24dp);
-
-            arrowBackAlphaAnimator.start();
         }
+
+        animateBase(findViewById(R.id.settings_layout), "backgroundColor",
+                oldBackgroundColorMode,
+                backgroundColorMode);
+        animateBase(findViewById(R.id.nmText), "textColor",
+                oldTextColorMode,
+                textColorMode);
+        animateBase(findViewById(R.id.actonbar_title), "textColor",
+                oldTextColorMode,
+                textColorMode);
+        animateBase(findViewById(R.id.reset_fuel_btn), "textColor",
+                oldTextColorMode,
+                textColorMode);
+        animateBase(findViewById(R.id.add_fuel_btn), "textColor",
+                oldTextColorMode,
+                textColorMode);
+        animateBase(findViewById(R.id.help_button), "textColor",
+                oldTextColorMode,
+                textColorMode);
+        animateBase(findViewById(R.id.about_button), "textColor",
+                oldTextColorMode,
+                textColorMode);
+
+        arrowBackAlphaAnimator.start();
     }
 
     public void onClickSwitch(View view) {
@@ -289,13 +251,11 @@ public class SettingsActivity extends AppCompatActivity
         finish();
     }
 
-
-
     public void onHelpButtonClick(View v) {
-
+        animateClick(v);
     }
 
     public void onAboutUsButtonClick(View v) {
-
+        animateClick(v);
     }
 }
